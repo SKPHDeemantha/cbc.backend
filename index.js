251a84f629e5b86c14883432c5bb6e6cbf4c4589
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2024-12-14 01:00:58 +0530

    connected Database and Server

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("My name is hshan");
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+
+const app =express();
+
+const mongourl ="mongodb+srv://Admin:1234@cluster0.xbgyb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(mongourl,{})
+
+const connection =mongoose.connection;
+
+connection.once("open",
+    ()=>{
+        console.log("Database Connected");
+    })
+
+    app.listen(
+        5000,
+        ()=>{
+            console.log("Server is running on port 5000");
+        }
+    )
