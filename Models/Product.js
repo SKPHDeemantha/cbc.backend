002b0a411233de25c3e44d677be9ccd39fa7a879
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2024-12-14 01:19:24 +0530

    product model created

---
+++ Models/Product.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const productSchema =mongoose.Schema({
+    ProductId :{
+        type : String,
+        required :true,
+        unique : true
+    },
+    ProductName :{
+        type : String,
+        required :true
+    },
+    altNames : [{
+        type : String,
+    }],
+    price : {
+        type : Number,
+        required : true,
+    },
+    lastPrice : {
+        type :Number,
+
+    },stock : {
+        type :Number,
+    },
+    description :{
+        type : String,
+    },
+    images : [{
+        type : String
+    }] 
+})
+
+const Product = mongoose.model("Products",productSchema)
+
+export default Product
